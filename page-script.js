6cd2edeebadd584a89324fe932e73318529fac86
vuthanhdat2k3 <vuthanhdat19052k3@gmail.com>
2024-03-07 13:37:43 +0700

    first commit

--- page-script.js
+++ page-script.js
@@ -5,63 +5,94 @@ function getInfoStudent() {
   var birthday = document.getElementById("Birthday").value;
   var CCCD = document.getElementById("ID").value;
   var address = document.getElementById("address").value;
-  $(".info-user-box").hide();
-  $("#start-exam").hide();
-  var info= "<h3>Kết quả</h3>" +
-  "<p>Họ và tên: " + name + "</p>" +
-  "<p>Ngày sinh: " + birthday + "</p>" +
-  "<p>CCCD: " + CCCD + "</p>" + 
-  "<p>Địa chỉ: " + address + "</p>";
-  return info;
+  if(name == "" || birthday == "" || CCCD == "" || address == ""){
+    alert("Vui lòng nhập đầy đủ thông tin trước khi bắt đầu bài thi!");
+    return "";
+  }
+  else{
+    $(".info-user-box").hide();
+    $("#start-exam").hide();
+    var info= "<h3>Thông tin</h3>" +
+    "<p>Họ và tên: " + name + "</p>" +
+    "<p>Ngày sinh: " + birthday + "</p>" +
+    "<p>CCCD: " + CCCD + "</p>" + 
+    "<p>Địa chỉ: " + address + "</p>";
+    return info;
+  }
 }
 
-function checkAnswer(){
+var selected = [];
+
+function checkAnswer() {
   var questions = document.getElementsByClassName("question");
   var correctAnswer = 0;
-  for(var i = 0 ; i < questions.length ; i++){
-   var radios = questions[i].querySelectorAll('input[type="radio"]');
-   var checkbox = questions[i].querySelectorAll('input[type="checkbox"]');
-   var userAnswer = null;
-   var answerN3 = [];
-   for(var j = 0 ; j < radios.length ; j++){
-    if(radios[j].checked){
-      userAnswer = radios[j].value;
-      break;
+  for (var i = 0; i < questions.length; i++) {
+    var radios = questions[i].querySelectorAll('input[type="radio"]');
+    var checkbox = questions[i].querySelectorAll('input[type="checkbox"]');
+    var userAnswer = null;
+    var answerN3 = [];
+    for (var j = 0; j < radios.length; j++) {
+      if (radios[j].checked) {
+        selected.push(radios[j].value); // Corrected pushing value into selected array
+        break;
+      }
     }
-   }
-   if(userAnswer === "true") {
-    correctAnswer ++;
-   }
-   for(var j = 0 ; j < checkbox.length ; j++){
-    if(checkbox[j].checked){
-      answerN3.push(checkbox[j].value);
+    for (var j = 0; j < checkbox.length; j++) {
+      if (checkbox[j].checked) {
+        answerN3.push(checkbox[j].value);
+      }
+      else{
+        answerN3.push(null);
+      }
     }
-    else {
-      if(checkbox[j].value === "true") answerN3.push("false");
+    if(answerN3.length > 0) selected.push(answerN3);
+    var textArea = questions[i].querySelector('textarea');
+    if (textArea !== null) {
+      var text = textArea.value.trim();
+      selected.push(text);
     }
-   }
-   if(!answerN3.includes("false") && answerN3.length > 0){
-    correctAnswer ++;
-   }
-   if(questions[i].querySelector('textarea') !== null){
-    var text = questions[i].querySelector('textarea').value.trim();
-    if(text !== ''){
-      correctAnswer ++;
-   }
-   }
+    if(selected.length < i + 1) selected.push(null);
   }
-  return correctAnswer;
 }
 
 function startExam() {
-  $(".info-user-box").hide();
-  $("#exam").show();
+  if(getInfoStudent() !== ""){
+    $(".info-user-box").hide();
+    $("#exam").show();
+  }
 }
 
 function submitResult(){
   var info = getInfoStudent();
   var result = checkAnswer();
+
   $(".content").hide();
   $(".result-box").show();
-  document.getElementById("result").innerHTML += info + "Kết quả: " + result + "/40";
+  document.getElementById("result").innerHTML += info;
+
+  console.log(selected);
+  var questions = document.getElementsByClassName('answer');
+  for(var i = 0 ; i < questions.length ; i++){
+    var radios = questions[i].querySelectorAll('input[type="radio"]');
+    var checkbox = questions[i].querySelectorAll('input[type="checkbox"]');
+    var textArea = questions[i].querySelector('textarea');
+
+    for(var j = 0 ; j < radios.length ; j++){
+      if(radios[j].value === selected[i]){
+        radios[j].checked = true;
+        break;
+      }
+    }
+    for (var j = 0; j < checkbox.length; j++) {
+      if(selected[i].length > 0){
+        if(checkbox[j].value === selected[i][j]){
+          checkbox[j].checked = true;
+        }
+      }
+    }
+
+    if (textArea !== null) {
+      textArea.value = selected[i];
+    }
+  }
 }
